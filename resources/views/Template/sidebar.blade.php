95fd7419efde2154b9d62ae6d5e54140659796ca
Rezky Azhar A <rezkyrnp@gmail.com>
2023-06-20 16:05:41 +0700

    sidebar kantor

--- resources/views/Template/sidebar.blade.php
+++ resources/views/Template/sidebar.blade.php
@@ -31,9 +31,7 @@
             <li class="nav-item {{ Request::routeIs('kantor') ? 'active' : '' }}">
                 <a href="{{route('kantor')}}">
                     <span class="icon">
-                        <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M17.4167 4.58333V6.41667H13.75V4.58333H17.4167ZM8.25 4.58333V10.0833H4.58333V4.58333H8.25ZM17.4167 11.9167V17.4167H13.75V11.9167H17.4167ZM8.25 15.5833V17.4167H4.58333V15.5833H8.25ZM19.25 2.75H11.9167V8.25H19.25V2.75ZM10.0833 2.75H2.75V11.9167H10.0833V2.75ZM19.25 10.0833H11.9167V19.25H19.25V10.0833ZM10.0833 13.75H2.75V19.25H10.0833V13.75Z" />
-                        </svg>
+                        <i class="lni lni-apartment"></i>
                     </span>
                     <span class="text">Kantor</span>
                 </a>
@@ -46,18 +44,7 @@
                         </svg>
                     </span>
                     <span class="text">Tiket</span>
-<<<<<<< HEAD
-=======
                 </a>
-            <li class="nav-item {{ Request::routeIs('kantor') ? 'active' : '' }}">
-                <a href="{{route('kantor')}}">
-                    <span class="icon">
-                        <i class="lni lni-home"></i>
-                    </span>
-                    <span class="text">Kantor</span>
->>>>>>> b70243a353986df73ca26421ff699cd3c6fa7f98
-                </a>
-            </li>
             @endif
         </ul>
     </nav>
